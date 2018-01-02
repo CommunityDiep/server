b7df452d3041c5124079b8d4b440bf48f4ea2850
haykam821 <haykam821@users.noreply.github.com>
2018-01-02 03:39:14 -0500

    Revert "Remove screen dimensions"
    This reverts commit e552e51e08422f3e0fc05407bec97dbd8c8c2ba5.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ let timer = 0;
 let ip_list = [];
 const ip_dic = {};
 const infolist = {};
+const dimensions = {};
 
 function speed(name) {
 	switch (name) {
@@ -354,6 +355,8 @@ class Shape {
         self.getUpdatePack = player => {
             const player_x = player.x;
             const player_y = player.y;
+            const screen_width = dimensions[`${player.id}width`];
+            const screen_height = dimensions[`${player.id}height`];
             if (Math.abs(player.x - self.x) < screen_width && Math.abs(player.y - self.y) < screen_height) {
                 return {
                     id: self.id,
@@ -789,6 +792,8 @@ io.sockets.on('connection', socket => {
 	});
 
 	socket.on('signIn', data => {
+		dimensions[`${socket.id}width`] = data.width;
+		dimensions[`${socket.id}height`] = data.height;
 		let username = data.name;
 		const tank_choice = data.tank;
 		username = username.slice(0, 16);
