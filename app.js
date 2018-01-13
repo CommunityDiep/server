f2a942a1af3d26a799e21cc7f0033e5fe181bf19
haykam821 <haykam821@users.noreply.github.com>
2018-01-13 14:23:33 -0500

    Change function call name thing

--- app.js
+++ app.js
@@ -433,7 +433,7 @@ class Player {
             b.y = self.y;
 						if (self.tank === "bomber" || self.tank === "grenadier") {
 							setTimeout(function() {
-								b.nineEleven(self.tank === "bomber" ? 5 : 8);
+								b.explode(self.tank === "bomber" ? 5 : 8);
 								b.toRemove = true;
 							}, 1000);
 						}}
